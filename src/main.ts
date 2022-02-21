37386604b892ac09efe449c3dd1559546876d18e
Antoine <contact@antt0n.tech>
2022-02-21 20:15:45 +0100

    Init Vue Project

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+// Custom import
+import VueErdJsPlugin from 'vue-erdjs'
+
+const app = createApp(App)
+
+app.use(router)
+
+//Vue.use(VueErdJsPlugin)
+
+app.mount('#app')
