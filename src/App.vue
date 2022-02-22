770460fa91a9a29ac69833612d31e2ed43f054ff
Antoine Seguin <contact@antoine-seguin.fr>
2022-02-22 03:08:08 +0100

    Update Creators  component - finished

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
@@ -10,97 +9,18 @@ import HelloWorld from '@/components/HelloWorld.vue'
 <style>
 @import '@/assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
+body {
+  min-height: 100vh;
+  font-family: "Roboto";
+  font-size: 15px;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 
-  font-weight: normal;
-}
+  background: center url(@/assets/images/backgrounds/background.png);
+  height: 100000px;
 
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/components/home/Creators.vue
+++ src/components/home/Creators.vue
@@ -11,18 +11,45 @@
 
   </div>
 
-  <div class="row">
+  <div class="row justify-content-md-center text-center">
 
-      <div class="col-md-4">
+      <div class="col-md-4 d-flex align-items-stretch">
+
+        <div class="card" style="background-color: rgba(255, 166, 65, 0.2);">
+          <img class="card-img-top" src="@/assets/images/creators/overgames.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title" id="overgames">@overgames</h5>
+            <hr/>
+            <p class="card-text">Passionate about programming and crypto. He takes care of the development of the discord bot as well as the communications parts.</p>
+          </div>
+        </div>
 
       </div>
 
-      <div class="col-md-4">
+      <div class="col-md-4 d-flex align-items-stretch">
+            
+        <div class="card" style="background-color: rgba(122, 25, 164, 0.2);">
+          <img class="card-img-top" src="@/assets/images/creators/timkyl.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title" id="timkyl">@timkyl2203</h5>
+            <hr/>
+            <p class="card-text">Passionate about crypto. He takes care of the communications of the project.</p>
+          </div>
+        </div>
             
       </div>
 
-      <div class="col-md-4">
+      <div class="col-md-4 d-flex align-items-stretch">
             
+        <div class="card" style="background-color: rgba(163, 24, 100, 0.2);">
+          <img class="card-img-top" src="@/assets/images/creators/torahime.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title" id="torahime">@torahime</h5>
+            <hr/>
+            <p class="card-text">Passionate about graphic design for 7 years. He takes care of the design of the eRobots and the general graphic appearance of the project.</p>
+          </div>
+        </div>
+
       </div>
 
   </div>
@@ -36,13 +63,59 @@ h1, h2 {
   font-family: "Robot";
   text-transform: uppercase;
   margin: 0px;
+  font-size: 35px;
 }
 h1 {
-  font-size: 72px;
+  font-size: 100px;
   background: linear-gradient(90deg, #0060ff, #00a8ff);
   -webkit-background-clip: text;
   background-clip: text;
   -webkit-text-fill-color: transparent;
-  margin-top: -10px;
+  margin-top: -20px;
+}
+.card {
+  margin: 25px;
+  border-radius: 25px;
+  border: none;
+}
+.card > img {
+  border-radius: 20px;
+}
+.card-title {
+
+  margin: -5px;
+  
+  font-size: 30px;
+  font-weight: 900;
+  text-transform: uppercase;
+}
+hr {
+  width: 28%;
+  margin: 10px auto;
+  border-top: 2px solid grey;
+  opacity: 80%;
+}
+/* Must duplicate code */
+#overgames {
+  background: linear-gradient(90deg, #2037e6, rgb(255, 166, 65));
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+#timkyl {
+  background: linear-gradient(90deg, rgb(122, 25, 164), #AE3EDE);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+#torahime {
+  background: linear-gradient(90deg, rgb(88, 27, 61), rgb(255, 0, 140));
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.card-text {
+  font-size: 19px;
+  line-height: 1.1;
 }
 </style>
